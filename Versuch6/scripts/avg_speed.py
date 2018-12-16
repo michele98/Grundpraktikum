c4f51bb191e7fd2580dd81efb69b0dd50065a649
michele98 <calvanese.michele@gmail.com>
2018-12-16 17:40:27 +0100

    boh

--- Versuch6/scripts/avg_speed.py
+++ Versuch6/scripts/avg_speed.py
@@ -23,8 +23,8 @@ def get_settings():
     general_sets.dataset_folder = "../datasets/"
     general_sets.dataset_file_name = "mittlere_geschwindigkeit.csv"
 
-    general_sets.x_label = r"$D^2 \:(m^2)$"
-    general_sets.y_label = r"$<v> (m/s)$"
+    general_sets.x_label = r"$X = D^2 \:(m^2)$"
+    general_sets.y_label = r"$y = \langle v \rangle \: (m/s)$"
 
     general_sets.graph_format = ["b.","r-","y--","y--"]
     #general_sets.fitted_graph_label = "Gefittete Kurve: " + r"$y = A\:\cos(\omega t + \phi)\: e^{-\beta t}$"
@@ -79,13 +79,13 @@ def plot_line(sets):
     a_str = fmtr.format("{0:.1u}",params[0])
     v_str = fmtr.format("{0:.1u}",params[1])
 
-    sets.graph_label[1] = "y= ax + v\n" + r"$a={}\:$".format(a_str) + r"$(ms)^{-1}\ und\ $" + r"$v={}\: m/s$".format(v_str)
+    sets.graph_label[1] = r"$y= kX + v$" + r"$\quad mit\quad y= \langle v \rangle \ und\ X=D^2$" +"\n"+ r"$k={}\:$".format(a_str) + r"$(ms)^{-1}\ und\ $" + r"$v={}\: m/s$".format(v_str)
     #fig, ax = plot_multi_2(sets, x_values = [distances2_n,x_fit,x_fit,x_fit], y_values = [v_avg_n,y_fit,y_fit_up,y_fit_down], x_err = [distances2_s,[],[],[]], y_err = [v_avg_s,[], [],[]])
     fig, ax = plot_multi_2(sets, x_values = [distances2_n,x_fit], y_values = [v_avg_n,y_fit], x_err = [distances2_s,[]], y_err = [v_avg_s,[]])
     ax.fill_between(x_fit,y_fit_up,y_fit_down, facecolor = "r", alpha = 0.1)
     ax.set_xbound(0,2.01)
     ax.set_ybound(0.5,0.5599)
-    plt.subplots_adjust(left=0.16, right = 0.97, bottom = 0.15, top = 0.99)
+    plt.subplots_adjust(left=0.162, right = 0.975, bottom = 0.15, top = 0.99)
     #ax.spines['top'].set_visible(False)
     #ax.spines['right'].set_visible(False)
 
